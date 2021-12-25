ec1955e6b89de85ab3135cffe03d870925b585cc
Henric Trotzig <henric.trotzig@gmail.com>
2021-12-25 20:39:07 +0100

    Add working p2p streaming
    Broadcaster go to /broadcast.html, viewers go to /

---
+++ public/broadcast.js
@@ -0,0 +1,74 @@
+const peerConnections = {};
+const config = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302"]
+    }
+  ]
+};
+
+const socket = io.connect(window.location.origin);
+const video = document.querySelector("video");
+
+// Media contrains
+const constraints = {
+  video: { facingMode: "user" }
+  // Uncomment to enable audio
+  // audio: true,
+};
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then(stream => {
+    console.log('stream available');
+    video.srcObject = stream;
+    socket.emit("broadcaster");
+  })
+  .catch(error => console.error(error));
+
+socket.on("watcher", id => {
+  console.log('watcher', id);
+  const peerConnection = new RTCPeerConnection(config);
+  peerConnections[id] = peerConnection;
+
+  let stream = video.srcObject;
+  stream.getTracks().forEach(track => {
+    console.log('track', track);
+    peerConnection.addTrack(track, stream);
+  });
+
+  peerConnection.onicecandidate = event => {
+    console.log('onicecandidate', event);
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
+
+  peerConnection
+    .createOffer()
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      socket.emit("offer", id, peerConnection.localDescription);
+    })
+    .catch((e) => console.error(e));
+});
+
+socket.on("answer", (id, description) => {
+  console.log('answer', id, description);
+  peerConnections[id].setRemoteDescription(description);
+});
+
+socket.on("candidate", (id, candidate) => {
+  console.log('candidate', id, candidate);
+  peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+});
+
+socket.on("disconnectPeer", id => {
+  peerConnections[id].close();
+  delete peerConnections[id];
+});
+
+window.onunload = window.onbeforeunload = () => {
+  socket.close();
+};
+
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const port = 4000;
+
+const http = require('http');
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('error', e => console.log(e));
+server.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`),
+);
+
+let broadcaster;
+
+io.sockets.on('connection', socket => {
+  console.log('connection established');
+  socket.on('broadcaster', () => {
+    console.log('broadcaster', socket.id);
+    broadcaster = socket.id;
+    socket.broadcast.emit('broadcaster');
+  });
+  socket.on('watcher', () => {
+    console.log('watcher', socket.id);
+    socket.to(broadcaster).emit('watcher', socket.id);
+  });
+  socket.on('disconnect', () => {
+    console.log('disconnect', socket.id);
+    socket.to(broadcaster).emit('disconnectPeer', socket.id);
+  });
+
+  socket.on('offer', (id, message) => {
+    console.log('offer', socket.id, message);
+    socket.to(id).emit('offer', socket.id, message);
+  });
+  socket.on('answer', (id, message) => {
+    console.log('answer', socket.id, message);
+    socket.to(id).emit('answer', socket.id, message);
+  });
+  socket.on('candidate', (id, message) => {
+    console.log('candidate', socket.id, message);
+    socket.to(id).emit('candidate', socket.id, message);
+  });
+});
