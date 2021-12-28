36f568df9dbd3cf5df19ef7fd4041ce99dc9d01c
Henric Trotzig <henric.trotzig@gmail.com>
2021-12-28 20:58:29 +0100

    Run prettier on broadcast.js

--- public/broadcast.js
+++ public/broadcast.js
@@ -2,17 +2,17 @@ const peerConnections = {};
 const config = {
   iceServers: [
     {
-      urls: ["stun:stun.l.google.com:19302"]
-    }
-  ]
+      urls: ['stun:stun.l.google.com:19302'],
+    },
+  ],
 };
 
 const socket = io.connect(window.location.origin);
-const video = document.querySelector("video");
+const video = document.querySelector('video');
 
 // Media contrains
 const constraints = {
-  video: { facingMode: "user" }
+  video: { facingMode: 'user' },
   // Uncomment to enable audio
   // audio: true,
 };
@@ -22,11 +22,11 @@ navigator.mediaDevices
   .then(stream => {
     console.log('stream available');
     video.srcObject = stream;
-    socket.emit("broadcaster", { broadcastId: window.broadcastId });
+    socket.emit('broadcaster', { broadcastId: window.broadcastId });
   })
   .catch(error => console.error(error));
 
-socket.on("watcher", id => {
+socket.on('watcher', id => {
   console.log('watcher', id);
   const peerConnection = new RTCPeerConnection(config);
   peerConnections[id] = peerConnection;
@@ -40,7 +40,7 @@ socket.on("watcher", id => {
   peerConnection.onicecandidate = event => {
     console.log('onicecandidate', event);
     if (event.candidate) {
-      socket.emit("candidate", id, event.candidate);
+      socket.emit('candidate', id, event.candidate);
     }
   };
 
@@ -48,22 +48,22 @@ socket.on("watcher", id => {
     .createOffer()
     .then(sdp => peerConnection.setLocalDescription(sdp))
     .then(() => {
-      socket.emit("offer", id, peerConnection.localDescription);
+      socket.emit('offer', id, peerConnection.localDescription);
     })
-    .catch((e) => console.error(e));
+    .catch(e => console.error(e));
 });
 
-socket.on("answer", (id, description) => {
+socket.on('answer', (id, description) => {
   console.log('answer', id, description);
   peerConnections[id].setRemoteDescription(description);
 });
 
-socket.on("candidate", (id, candidate) => {
+socket.on('candidate', (id, candidate) => {
   console.log('candidate', id, candidate);
   peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
 });
 
-socket.on("disconnectPeer", id => {
+socket.on('disconnectPeer', id => {
   const peerConnection = peerConnections[id];
   if (peerConnection) {
     peerConnection.close();
@@ -74,4 +74,3 @@ socket.on("disconnectPeer", id => {
 window.onunload = window.onbeforeunload = () => {
   socket.close();
 };
-
