828b8a1b92fbdc64158fbd31221bdf746547fc66
Henric Trotzig <henric.trotzig@gmail.com>
2021-12-30 10:00:23 +0100

    Add next.js
    I'm going to use this as the framework building out the app from now on.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const crypto = require('crypto');
 
+const next = require('next');
+
 const express = require('express');
 const app = express();
 
@@ -10,11 +12,15 @@ const server = http.createServer(app);
 
 const io = require('socket.io')(server);
 
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const nextHandle = nextApp.getRequestHandler();
+
 const broadcasters = {
   // mapping id => socketId
 };
 
-app.use((req, res, next) =>  {
+app.use((req, res, next) => {
   if (`${port}` === '80' && !req.secure) {
     return res.redirect('https://' + req.headers.host + req.url);
   }
@@ -25,59 +31,65 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 io.sockets.on('error', e => console.error(e));
-server.listen(port, () =>
-  console.log(`Server is running on http://localhost:${port}`),
-);
+nextApp.prepare().then(() => {
+  server.listen(port, () =>
+    console.log(`Server is running on http://localhost:${port}`),
+  );
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-app.get('/broadcast', (req, res, next) => {
-  res.render('broadcast', {
-    broadcastId: crypto.randomBytes(2).toString('hex'),
+  app.get('/', (req, res, next) => {
+    res.render('index');
   });
-});
 
-app.get('/watch', (req, res, next) => {
-  const { broadcastId } = req.query;
-  if (!broadcasters[broadcastId]) {
-    return res
-      .status(404)
-      .render('index', { ...req.query, error: 'not_found' });
-  }
-  res.render('watch', req.query);
-});
-
-io.sockets.on('connection', socket => {
-  socket.on('broadcaster', event => {
-    broadcasters[event.broadcastId] = socket.id;
-    socket.broadcast.emit('broadcaster');
+  app.get('/broadcast', (req, res, next) => {
+    res.render('broadcast', {
+      broadcastId: crypto.randomBytes(2).toString('hex'),
+    });
   });
-  socket.on('watcher', event => {
-    const broadcaster = broadcasters[event.broadcastId];
-    if (broadcaster) {
-      socket.to(broadcaster).emit('watcher', socket.id);
+
+  app.get('/watch', (req, res, next) => {
+    const { broadcastId } = req.query;
+    if (!broadcasters[broadcastId]) {
+      return res
+        .status(404)
+        .render('index', { ...req.query, error: 'not_found' });
     }
+    res.render('watch', req.query);
   });
-  socket.on('disconnect', event => {
-    for (const socketId of Object.values(broadcasters)) {
-      socket.to(socketId).emit('disconnectPeer', socket.id);
-    }
-    for (const broadcastId of Object.keys(broadcasters)) {
-      if (broadcasters[broadcastId] === socket.id) {
-        delete broadcasters[broadcastId];
+
+  app.get('*', (req, res) => {
+    return nextHandle(req, res);
+  });
+
+  io.sockets.on('connection', socket => {
+    socket.on('broadcaster', event => {
+      broadcasters[event.broadcastId] = socket.id;
+      socket.broadcast.emit('broadcaster');
+    });
+    socket.on('watcher', event => {
+      const broadcaster = broadcasters[event.broadcastId];
+      if (broadcaster) {
+        socket.to(broadcaster).emit('watcher', socket.id);
       }
-    }
-  });
+    });
+    socket.on('disconnect', event => {
+      for (const socketId of Object.values(broadcasters)) {
+        socket.to(socketId).emit('disconnectPeer', socket.id);
+      }
+      for (const broadcastId of Object.keys(broadcasters)) {
+        if (broadcasters[broadcastId] === socket.id) {
+          delete broadcasters[broadcastId];
+        }
+      }
+    });
 
-  socket.on('offer', (id, message) => {
-    socket.to(id).emit('offer', socket.id, message);
-  });
-  socket.on('answer', (id, message) => {
-    socket.to(id).emit('answer', socket.id, message);
-  });
-  socket.on('candidate', (id, message) => {
-    socket.to(id).emit('candidate', socket.id, message);
+    socket.on('offer', (id, message) => {
+      socket.to(id).emit('offer', socket.id, message);
+    });
+    socket.on('answer', (id, message) => {
+      socket.to(id).emit('answer', socket.id, message);
+    });
+    socket.on('candidate', (id, message) => {
+      socket.to(id).emit('candidate', socket.id, message);
+    });
   });
 });
