494e2b4bfa670ba227abec4e1c5a2aad04da85e7
Henric Trotzig <henric.trotzig@gmail.com>
2022-01-02 01:08:22 +0100

    Look good on iOS devices
    Adding some stuff from this excellent guide:
    https://johanbrook.com/writings/ios-homescreen-web-app/

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,14 @@ function SwingMirrorApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon.ico"/>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, user-scalable=no, viewport-fit=cover"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-icon-60@2x.png" />
+        <link rel="icon" href="/favicon.ico" />
         <title>Swing Mirror</title>
       </Head>
       <Component {...pageProps} />;
