ebcd76434b8ff33b998f313547e285c4c585325a
Henric Trotzig <henric.trotzig@gmail.com>
2022-01-07 22:20:12 +0100

    Render share button on translucent circle

--- src/ShareButton.js
+++ src/ShareButton.js
@@ -15,27 +15,29 @@ export default function ShareButton({ video }) {
     return null;
   }
 
-  if (type === 'share') {
+  if (type === 'share' || true) {
     return (
-      <button
-        className="reset"
-        disabled={!video}
-        onClick={async () => {
-          const res = await fetch(video.url);
-          const blob = await res.blob();
-          const file = new File([blob], `video.${fileSuffix(blob.type)}`, {
-            type: blob.type,
-          });
-          if (navigator.canShare && navigator.canShare({ files: [file] })) {
-            await navigator.share({
-              title: 'Swing Mirror video',
-              files: [file],
+      <div className="rounded-translucent">
+        <button
+          className="reset"
+          disabled={!video}
+          onClick={async () => {
+            const res = await fetch(video.url);
+            const blob = await res.blob();
+            const file = new File([blob], `video.${fileSuffix(blob.type)}`, {
+              type: blob.type,
             });
-          }
-        }}
-      >
-        <Share />
-      </button>
+            if (navigator.canShare && navigator.canShare({ files: [file] })) {
+              await navigator.share({
+                title: 'Swing Mirror video',
+                files: [file],
+              });
+            }
+          }}
+        >
+          <Share />
+        </button>
+      </div>
     );
   }
 
