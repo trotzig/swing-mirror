dba9362d4b4d310e59d98bff4079022e11358211
Henric Trotzig <henric.trotzig@gmail.com>
2022-01-07 22:41:02 +0100

    Simplify VideoInfo
    Instead of rendering a button and an input, we can just always render
    the input.

--- src/VideoInfo.js
+++ src/VideoInfo.js
@@ -3,41 +3,24 @@ import React, { useEffect, useRef, useState } from 'react';
 import db from './db';
 
 export default function VideoInfo({ video }) {
-  const [isEditing, setIsEditing] = useState(false);
   const [value, setValue] = useState(video.name);
-  const inputRef = useRef();
-
-  useEffect(() => {
-    if (!isEditing) {
-      return;
-    }
-    inputRef.current.focus();
-    inputRef.current.select();
-  }, [isEditing]);
 
   return (
     <div className="video-info">
-      {isEditing ? (
-        <input
-          ref={inputRef}
-          type="text"
-          value={value}
-          onChange={e => setValue(e.target.value)}
-          onKeyDown={e => {
-            if (e.which === 13) {
-              e.target.blur();
-            }
-          }}
-          onBlur={async () => {
-            setIsEditing(false);
-            await db.updateVideoName(video.id, value);
-          }}
-        />
-      ) : (
-        <button className="reset" onClick={() => setIsEditing(true)}>
-          {value || 'Unnamed video'}
-        </button>
-      )}
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        onFocus={e => e.target.select()}
+        onKeyDown={e => {
+          if (e.which === 13) {
+            e.target.blur();
+          }
+        }}
+        onBlur={async () => {
+          await db.updateVideoName(video.id, value);
+        }}
+      />
     </div>
   );
 }
