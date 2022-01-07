8a0e06032adae6c9d3a7644bd2ae8504a1bb01c0
Henric Trotzig <henric.trotzig@gmail.com>
2022-01-07 12:13:56 +0100

    Change feature-detection to userAgent in videoDimensions
    This will make things work better on Chromebooks and Windows machines.

--- pages/broadcast.js
+++ pages/broadcast.js
@@ -12,7 +12,7 @@ import VideoRecorder from '../src/VideoRecorder';
 import db from '../src/db';
 
 function videoDimensions(videoEl) {
-  if ('ontouchstart' in window) {
+  if (/iPad|iPhone|iPod|android/.test(navigator.userAgent)) {
     // iOS and Android constraints are expressed in landscape mode, so we need
     // to flip width and height
     // https://stackoverflow.com/questions/62538271/getusermedia-selfie-full-screen-on-mobile
