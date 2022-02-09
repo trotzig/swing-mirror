c85e7428f7900f9f6a56663ac57c91e1b135fa14
Henric Trotzig <henric.trotzig@gmail.com>
2022-02-09 08:54:21 +0100

    Add IndexPage storybook story
    This is the first of a few I'm adding.

---
+++ src/stories/IndexPage.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IndexPage from '../../pages/index';
+import '../../public/styles.css';
+import '../../public/range-input.css';
+
+const config = {
+  component: IndexPage,
+};
+
+export default config;
+
+export const Default = () => <IndexPage />;
